586e5231e062eafa5c6ad3156261d2778bfd8584
ArtyomKF <Artemmailsbox@gmail.com>
2021-02-10 17:32:09 +0500

    Implement task 5

--- src/gui/maps_app.py
+++ src/gui/maps_app.py
@@ -6,10 +6,10 @@ from PyQt5.QtWidgets import QMainWindow, qApp, QVBoxLayout
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 
 
-SCREEN_SIZE = [600, 450]
+SCREEN_SIZE = [600, 500]
 
 
 class MapsApp(QMainWindow):
@@ -30,15 +30,15 @@ class MapsApp(QMainWindow):
     def get_image(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(self.coord)}&spn=0.002,0.002&" \
                       f"scale={self.scale}&l=map"
-        response = requests.get(map_request)
+        self.response = requests.get(map_request)
 
-        if not response:
+        if not self.response:
             print("Ошибка выполнения запроса:")
             print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print("Http статус:", self.response.status_code, "(", self.response.reason, ")")
             qApp.exit(1)
         with open(self.map_file, "wb") as file:
-            file.write(response.content)
+            file.write(self.response.content)
 
     def init_ui(self):
         main_layout = QVBoxLayout()
@@ -46,7 +46,21 @@ class MapsApp(QMainWindow):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.image.resize(600, 450)
 
+        self.btn = QPushButton('Принять', self)
+        self.btn.setFocusPolicy(Qt.NoFocus)
+
+        self.btn.resize(100, 25)
+        self.btn.move(500, 462)
+        self.btn.clicked.connect(self.place_find)
+
+        self.text = QLineEdit(self)
+        self.text.setFocusPolicy(Qt.NoFocus)
+
+        self.text.resize(350, 25)
+        self.text.move(0, 462)
+
         main_layout.addWidget(self.image)
+        main_layout.addWidget(self.btn)
 
         self.setLayout(main_layout)
         self.setWindowTitle('Maps App')
@@ -75,6 +89,25 @@ class MapsApp(QMainWindow):
         elif key == Qt.Key_PageDown:
             if self.scale > 1:
                 self.scale -= 0.1
+
+        self.get_image()
+        self.change_image()
+
+    def place_find(self):
+        toponym_to_find = self.text.text()
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+
+        self.coord = toponym_coodrinates.split()
+        self.scale = 1.0
 
         self.get_image()
         self.change_image()

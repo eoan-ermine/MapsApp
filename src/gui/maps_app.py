23f89d2594fcf010a46e8ac53599a1cc6045d822
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-10 17:21:37 +0500

    Implement task four

--- src/gui/maps_app.py
+++ src/gui/maps_app.py
@@ -2,7 +2,7 @@ import os
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, qApp, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, qApp, QVBoxLayout, QHBoxLayout, QPushButton
 
 import requests
 from PyQt5.QtGui import QPixmap
@@ -23,13 +23,15 @@ class MapsApp(QMainWindow):
         self.coord = ["37.530887", "55.70311"]
         self.scale = 1.0
 
+        self.layer = "map"
+
         self.get_image()
         self.init_ui()
         self.change_image()
 
     def get_image(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(self.coord)}&spn=0.002,0.002&" \
-                      f"scale={self.scale}&l=map"
+                      f"scale={self.scale}&l={self.layer}"
         response = requests.get(map_request)
 
         if not response:
@@ -40,17 +42,41 @@ class MapsApp(QMainWindow):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
+    def set_layer(self, layer_name):
+        self.layer = layer_name
+        self.get_image()
+        self.change_image()
+
     def init_ui(self):
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
 
         main_layout = QVBoxLayout()
 
+        select_view_layout = QHBoxLayout()
+        select_view_layout.addWidget(QLabel("Выбрать слой: "))
+
+        self.scheme_btn = QPushButton("Схема")
+        self.scheme_btn.clicked.connect(lambda e: self.set_layer("map"))
+        self.scheme_btn.setFocusPolicy(Qt.NoFocus)
+
+        self.satellite_btn = QPushButton("Спутник")
+        self.satellite_btn.clicked.connect(lambda e: self.set_layer("sat"))
+        self.satellite_btn.setFocusPolicy(Qt.NoFocus)
+
+        self.hybrid_btn = QPushButton("Гибрид")
+        self.hybrid_btn.clicked.connect(lambda e: self.set_layer("sat,skl"))
+        self.hybrid_btn.setFocusPolicy(Qt.NoFocus)
+
+        select_view_layout.addWidget(self.scheme_btn)
+        select_view_layout.addWidget(self.satellite_btn)
+        select_view_layout.addWidget(self.hybrid_btn)
+
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.image.resize(600, 450)
 
+        main_layout.addLayout(select_view_layout)
         main_layout.addWidget(self.image)
-
 
         main_widget.setLayout(main_layout)
         self.setWindowTitle('Maps App')
